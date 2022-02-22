a611c8e8e7d6e1f3010c615b707ba7088dcccb7f
MiriamRiefel <31964481+MiriamRiefel@users.noreply.github.com>
2022-02-22 17:52:42 +0100

    Add files via upload

---
+++ MINDYOURSELF_LEDcontrol.ino
@@ -0,0 +1,117 @@
+// A basic everyday NeoPixel strip test program.
+
+// NEOPIXEL BEST PRACTICES for most reliable operation:
+// - Add 1000 uF CAPACITOR between NeoPixel strip's + and - connections.
+// - MINIMIZE WIRING LENGTH between microcontroller board and first pixel.
+// - NeoPixel strip's DATA-IN should pass through a 300-500 OHM RESISTOR.
+// - AVOID connecting NeoPixels on a LIVE CIRCUIT. If you must, ALWAYS
+//   connect GROUND (-) first, then +, then data.
+// - When using a 3.3V microcontroller with a 5V-powered NeoPixel strip,
+//   a LOGIC-LEVEL CONVERTER on the data line is STRONGLY RECOMMENDED.
+// (Skipping these may work OK on your workbench but can fail in the field)
+
+#include <Adafruit_NeoPixel.h>
+#include "StringSplitter.h"
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN    6
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 135
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+String incomingByte;
+int r, g, b, wait;
+
+// setup() function -- runs once at startup --------------------------------
+
+void setup() {
+  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
+  // Any other board, you can remove this part (but no harm leaving it):
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+  Serial.begin(9600);
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(170); // Set BRIGHTNESS to about 1/5 (max = 255)
+}
+
+// loop() function -- runs repeatedly as long as board is on ---------------
+
+void loop() {
+  if (Serial.available() > 0) {
+    incomingByte = Serial.readStringUntil('\n');
+    
+    if (incomingByte == "stop") {
+      colorWipe(strip.Color(0, 0, 0), wait);
+    }
+
+   else {
+      String R = getValue(incomingByte, ':', 0);
+      r = R.toInt();
+      String G = getValue(incomingByte, ':', 1);
+      g = G.toInt();
+      String B = getValue(incomingByte, ':', 2);
+      b = B.toInt();
+      String W = getValue(incomingByte, ':', 3);
+      wait = W.toInt();
+      
+      while (Serial.available() == 0) {
+          colorWipe(strip.Color(r, g, b), wait);
+          colorWipe(strip.Color(abs(r-30), abs(g-30), abs(b-30)), wait);
+      }
+    }
+  }
+}
+
+
+// Some functions of our own for creating animated effects -----------------
+
+// Fill strip pixels one after another with a color. Strip is NOT cleared
+// first; anything there will be covered pixel by pixel. Pass in color
+// (as a single 'packed' 32-bit value, which you can get by calling
+// strip.Color(red, green, blue) as shown in the loop() function above),
+// and a delay time (in milliseconds) between pixels.
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    int r = random(2);
+    if(r==0){
+      strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    }
+  }
+  strip.show();    //  Update strip to match
+  delay(wait * 3);
+}
+
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
---
+++ MINDYOURSELF_controlscript_main_controlcondition.py
@@ -0,0 +1,389 @@
+#%% IMPORTS
+from pylsl import StreamInlet, resolve_stream
+import rtmidi
+import time
+import pandas as pd
+import numpy as np
+import random
+import serial
+from datetime import datetime
+#%% LOAD IN DATA of music samples and corresponding MIDI notes and corresponding val and ar
+df = pd.read_csv("C:/Users/s2365332/Downloads/VAL_AR.csv")
+
+df_note = df.iloc[0:60, 3].fillna(0).astype(int)
+df_val = df.iloc[0:60, 13].fillna(0).astype(int)
+df_ar = df.iloc[0:60, 14].fillna(0).astype(int)
+
+df_note = np.reshape(np.array(df_note), (15, 4))
+df_val = np.reshape(np.array(df_val), (15, 4))
+df_ar = np.reshape(np.array(df_ar), (15, 4))
+
+#%% LOAD IN FAKE EEG
+
+df_fake_eeg = np.array(pd.read_excel("fake_participant.xlsx"))
+
+#%% FUNCTION DEFINITIONS
+
+
+def max_min_int(value):  # Taking out extreme values and rounding the features
+    if value < -9:
+        value = -9
+    elif value > 10:
+        value = 10
+    else:
+        value = int(value)
+    return value
+
+
+def lower_arousal(list_of_indices, error):
+    for i in range(abs(error)):
+        if not(sum(list_of_indices[:3]) == 0):
+            action = 'percussion'
+        else:
+            action = 'melody'
+        if action == 'percussion':
+            options = np.nonzero(list_of_indices[:3])[0]
+            if len(options) > 0:
+                index = np.random.choice(options)
+                list_of_indices[index] -= 1
+            else:
+                list_of_indices = list_of_indices
+
+        elif action == 'melody':
+            options = np.nonzero(list_of_indices[3:])[0] + 3
+            if len(options) > 0:
+                index = np.random.choice(options)
+                list_of_indices[index] = 0
+            else:
+                list_of_indices = list_of_indices
+    return list_of_indices
+
+
+def raise_arousal(list_of_indices, error):
+    for i in range(error):
+        if sum(list_of_indices[:3]) == 9:
+            action = 'melody'
+        elif len(np.nonzero(list_of_indices[3:])[0]) == 12:
+            action = 'percussion'
+        else:
+            action = random.choice(['percussion', 'melody'])
+        if action == 'percussion':
+            options = np.where(list_of_indices[:3] < 3)[0]
+            if len(options) > 0:
+                index = np.random.choice(options)
+                list_of_indices[index] += 1
+            else:
+                list_of_indices = list_of_indices
+
+        elif action == 'melody':
+            options = np.where(list_of_indices[3:] == 0)[0] + 3
+            if len(options) > 0:
+                index = np.random.choice(options)
+                list_of_indices[index] = random.choice([1, 2, 3])
+            else:
+                list_of_indices = list_of_indices
+    return list_of_indices
+
+
+def change_arousal(list_of_indices, goal_arousal):
+    current_arousal = int(track_arousal_weight * (np.count_nonzero(list_of_indices[3:]) + np.sum(list_of_indices[:3])))
+    clipped_goal_arousal = np.clip(np.array([goal_arousal]), -9, 10)[0]
+    error = (clipped_goal_arousal + 10) - current_arousal
+
+    if error > 0:
+        ar_updated_list_of_indices = raise_arousal(list_of_indices, error)
+
+    elif error < 0:
+        ar_updated_list_of_indices = lower_arousal(list_of_indices, error)
+
+    else:
+        ar_updated_list_of_indices = list_of_indices
+
+    return ar_updated_list_of_indices
+
+
+def lower_valence(list_of_indices, error):
+    for i in range(abs(error)):
+        options = np.where(list_of_indices[3:] > 1)[0] + 3
+        if len(options)> 0:
+            index = np.random.choice(options)
+            list_of_indices[index] -= 1
+        else:
+            list_of_indices = list_of_indices
+    return list_of_indices
+
+
+def raise_valence(list_of_indices, error):
+    for i in range(error):
+        low_enough = list_of_indices[3:] < 3
+        high_enough = list_of_indices[3:] > 0
+        if (len(low_enough) > 0) and (len(high_enough) > 0):
+            options = np.where(high_enough & low_enough)[0] + 3
+            if len(options) > 0:
+                index = np.random.choice(options)
+                list_of_indices[index] += 1
+            else:
+                list_of_indices = list_of_indices
+        else:
+            list_of_indices = list_of_indices
+    return list_of_indices
+
+
+def change_valence(list_of_indices, goal_valence):
+    clipped_goal_valence = np.clip(np.array([goal_valence]), -10, 10)[0]
+    valence_on = list_of_indices[np.nonzero(list_of_indices[3:])[0] + 3]
+    if len(valence_on) > 0:
+        current_valence = np.mean((valence_on - 2) * 10)
+        error = clipped_goal_valence - current_valence
+        repetitions = int(error * 1.2)
+
+        if error > 0:
+            val_updated_list_of_indices = raise_valence(list_of_indices, repetitions)
+
+        elif error < 0:
+            val_updated_list_of_indices = lower_valence(list_of_indices, repetitions)
+
+        else:
+            val_updated_list_of_indices = list_of_indices
+    else:
+        val_updated_list_of_indices = list_of_indices
+
+    return val_updated_list_of_indices
+
+
+def indices_to_notes(list_of_indices):
+    list_of_notes = np.zeros(15)
+
+    for i, j in enumerate(list_of_indices):
+        list_of_notes[i] = df_note[i][j]
+
+    return list_of_notes
+
+
+def update_list_of_indices(list_of_indices, list_of_changes):
+    updated_list_of_indices = list_of_indices
+
+    for note in list_of_changes:
+        location, number = np.where(df_note == note)
+        updated_list_of_indices[location - 1] = number
+
+    return updated_list_of_indices
+
+
+def update_notes(list_of_indices, goal_arousal, goal_valence):
+    old_list_of_notes = indices_to_notes(list_of_indices)
+    ar_updated_list_of_indices = change_arousal(list_of_indices, goal_arousal)
+    val_updated_list_of_indices = change_valence(ar_updated_list_of_indices, goal_valence)
+    new_list_of_notes = indices_to_notes(val_updated_list_of_indices)
+    change_notes = new_list_of_notes[~(new_list_of_notes == old_list_of_notes)]
+    if len(change_notes) > max_changes:
+        downsized_list_of_changes = np.random.choice(change_notes, max_changes, replace=False)
+    else:
+        downsized_list_of_changes = change_notes
+
+    return downsized_list_of_changes
+
+
+def fetch_port_number():
+    midi_out = rtmidi.MidiOut()
+    available_ports = midi_out.get_ports()
+    print(available_ports)
+    del midi_out
+    return
+
+
+def send_midi(list_of_notes):
+    for note in list_of_notes:
+        midi_out = rtmidi.MidiOut()
+        available_ports = midi_out.get_ports()
+
+        if available_ports:
+            midi_out.open_port(midiport)
+        else:
+            midi_out.open_virtual_port("My virtual output")
+
+        with midi_out:
+            note_on = [0x90, note, 112] # channel 1, middle C, velocity 112
+            note_off = [0x80, note, 0]
+            midi_out.send_message(note_on)
+            time.sleep(0.5)
+            midi_out.send_message(note_off)
+            time.sleep(0.1)
+
+        del midi_out
+    return
+
+
+def send_cc_midi(list_of_values):
+    for i, value in enumerate(list_of_values):
+        midi_out = rtmidi.MidiOut()
+        available_ports = midi_out.get_ports()
+
+        if available_ports:
+            midi_out.open_port(midiport)
+        else:
+            midi_out.open_virtual_port("My virtual output")
+
+        with midi_out:
+            # MIDI CC = [.. , tempo, volume, reverb, underwater]
+            cc = [0xB0, (i + 1), value]
+            midi_out.send_message(cc)
+            time.sleep(0.5)
+
+        del midi_out
+    return
+
+
+def update_cc(old_cc, goal_cc):
+    new_cc = np.array([0, 0, 0, 0, 0])
+    for i, value in enumerate(old_cc[0]):
+        error = np.clip((goal_cc[i] - value), -max_midi_change, max_midi_change)
+        new_cc[i] = int(value + error)
+
+    return new_cc
+
+
+def update_rgb(old_rgb, goal_rgb):
+    new_rgb = np.array([0, 0, 0, 0])
+
+
+
+    for i, value in enumerate(old_rgb[0]):
+        error = np.clip((goal_rgb[i] - value), -max_light_change, max_light_change)
+        new_rgb[i] = int(value + error)
+
+    lowest = np.amin(new_rgb[0:3])
+    lowest_loc = np.where(new_rgb == lowest)
+    new_rgb[lowest_loc] = lowest/extremizer
+
+    highest = np.amax(new_rgb[0:3])
+    highest_loc = np.where(new_rgb == highest)
+    new_rgb[highest_loc] = highest * extremizer
+
+    new_rgb = np.clip(new_rgb, 0, 255)
+    return new_rgb
+
+
+def change_LEDs(rgb):
+    red, green, blue, wait = rgb
+    message = str(red) + ":" + str(green) + ":" + str(blue) + ":" + str(wait)
+    serialcomm.write(message.encode())
+    print("changing LEDs to: " + message)
+    return
+
+
+#%% MAIN UPDATE LOOP
+
+def main():
+    data = dict()
+    data['info'] = ['ar', 'val', 'fru', 'eng', 'val', 'exc', 'foc', 'med']
+
+    # first resolve an EEG stream on the lab network
+    print("looking for an EEG stream...")
+    stream_arval = resolve_stream('name', 'Arousal-Valence')
+    stream_perfmet = resolve_stream('name', 'EmotivDataStream-Performance-Metrics')
+
+    # create a new inlet to read from the stream
+    inlet_arval = StreamInlet(stream_arval[0])
+    inlet_perfmet = StreamInlet(stream_perfmet[0])
+
+    # some defaults to start with
+    list_of_indices = np.array([0, 0, 0, 1, 2, 0, 0, 1, 0, 3, 0, 0, 2, 3, 0])
+    real_ar, real_valence = [0, 0]
+    fru, eng, val, exc, foc, med = [0.2, 0.5, 0.5, 0.5, 0.5, 0.5]
+
+    initial_soundscape = indices_to_notes(list_of_indices)
+    send_midi(initial_soundscape)
+
+    new_rgb = np.array([100, 100, 100, 100])
+    new_cc_values = np.array([50, 50, 50, 50, 50])
+
+    i = 0
+
+    try:
+        while True:
+            # get a new sample (you can also omit the timestamp part if you're not
+            # interested in it)
+            chunk_arval, timestamps_arval = inlet_arval.pull_chunk()
+            chunk_perfmet, timestamps_perfmet = inlet_perfmet.pull_chunk()
+
+            if timestamps_arval:
+                real_ar = max_min_int(chunk_arval[0][0])  # arousal extracted via OpenVibe
+                real_valence = max_min_int(chunk_arval[0][1])  # valence extracted via OpenVibe
+                print("real arousal: " + str(real_ar) + ", real valence: " + str(real_valence))
+
+                fake_ar = int(df_fake_eeg[i][0])
+                fake_valence = int(df_fake_eeg[i][1])
+
+                old_list_of_indices = np.array(list_of_indices)
+                list_of_changes = update_notes(list_of_indices, fake_ar, fake_valence)
+
+                send_midi(list_of_changes)
+                list_of_indices = update_list_of_indices(old_list_of_indices, list_of_changes)
+
+            if timestamps_perfmet:
+                old_rgb = np.array([new_rgb])
+                old_cc_values = np.array([new_cc_values])
+
+                real_perfmet = np.array(chunk_perfmet[0][1:])
+                print("real perfmet: " + str(real_perfmet))
+
+
+                now = datetime.now()
+                data[now] = [real_ar, real_valence, real_perfmet]
+
+                fru, eng, val, exc, foc, med = df_fake_eeg[i][2:]
+
+                print("fake perfmet: ")
+                print("fru: " + str(fru) + ", val: " + str(val) + ", exc: " + str(exc)
+                      + ", foc: " + str(foc) + ", med: " + str(med))
+
+                goal_rgb = np.array([fru * 0.8, foc, med, (1-fru)]) * 255
+                goal_cc = np.array([fru, exc, (1-fru), foc, med]) * 127
+
+                new_rgb = update_rgb(old_rgb, goal_rgb)
+                new_cc_values = update_cc(old_cc_values, goal_cc)
+
+                change_LEDs(new_rgb)
+                send_cc_midi(new_cc_values)
+
+                i += 1
+
+    except KeyboardInterrupt:
+        #When the program is stopped, the data is written in a CSV file.
+        df = pd.DataFrame.from_dict(data, orient='index')
+        df.to_csv(('placebo_participant' + str(participant_number) +'.csv'))
+
+#%% reset serial port
+serialcomm = serial.Serial('COM4', 9600)
+serialcomm.close()
+
+
+#%% CONSTANTS
+fetch_port_number()  # check if the right MIDI port indeed still is 2 (LoopMIDI Port)
+midiport = 1
+
+max_changes = 7  # determine the maximum number of changing samples per time step, for the sake of continuity
+track_arousal_weight = 1.5 # the weight of the tracks to calculate arousal.
+
+max_light_change = 200
+max_midi_change = 60
+# If higher, less tracks will be activated simultaneously.
+
+extremizer = 1.5
+
+serialcomm = serial.Serial('COM4', 9600)
+serialcomm.timeout = 1
+
+participant_number = 35
+
+
+
+#%% RUN THE WHOLE SHABANG
+
+if __name__ == '__main__':
+    main()
+
+
+
+
